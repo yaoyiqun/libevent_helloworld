928709bf77d70e9a42d4d4eedef2f26b745bbed8
Leo Yao <yaoyiqun@outlook.com>
2022-01-22 23:18:19 +0800

    初始化基本框架，增加gitignore

---
+++ main.cpp
@@ -0,0 +1,50 @@
+#include <iostream>
+#include <event2/event.h>
+#include <csignal>
+#include <event2/listener.h>
+
+#define SPORT 5001
+
+void listen_cb(evconnlistener *base,
+               int cb, sockaddr *ptr, int flags, void *backlog
+){
+    std::cout<<"listen_cb"<<std::endl;
+}
+int main() {
+#ifdef _WIN32
+    std::cout << "Hello, windows World!" << std::endl;
+#elif __APPLE__
+    std::cout << "Hello, mac World!" << std::endl;
+#elif __linux__
+    std::cout << "Hello, linux World!" << std::endl;
+    if(signal(SIGPIPE,SIG_IGN)==SIG_ERR)
+#endif
+    std::cout << "Hello, World!" << std::endl;
+    event_base * base = event_base_new();
+    if(base!= nullptr)
+    {
+        std::cout<<"event_base_new success!"<<std::endl;
+    }
+    //监听端口
+    //socket,bind,listen绑定时间
+    sockaddr_in sin;
+    memset(&sin,0,sizeof(sin));
+    sin.sin_family = AF_INET;
+    sin.sin_port = htons(SPORT);
+    evconnlistener* ev= evconnlistener_new_bind(base,   //libevent的上下文
+                            listen_cb,//接收到连接的回调函数
+                            base,   //回调函数获取的参数arg
+                            LEV_OPT_REUSEABLE|LEV_OPT_CLOSE_ON_FREE,//地址重用，evconnlistener关闭同时关闭socket
+                            10, //链接队列大小，对应listen函数
+                            (sockaddr*)&sin,    //绑定的地址和端口
+                            sizeof(sin)
+            );
+    //事件分发处理
+    if(base)
+        event_base_dispatch(base);
+    if(ev)
+        evconnlistener_free(ev);
+    if(base)
+        event_base_free(base);
+    return 0;
+}
